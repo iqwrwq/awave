3d92cd6983987e4a99fab618d5703d8f4b6be6ca
= <=>
2022-06-13 17:05:41 +0200

    feat!(design): redesign whole application/n/nNew Design for awave

--- .aw/src/controller/config/Config.php
+++ .aw/src/controller/config/Config.php
@@ -7,22 +7,11 @@ use Symfony\Component\Dotenv\Dotenv;
 class Config
 {
     private Dotenv $dotEnv;
-    private string $theme;
 
     public function __construct()
     {
         $this->dotEnv = new Dotenv();
         $this->dotEnv->load( getcwd() . '/.aw/src/.env');
-        $this->theme = $_ENV['THEME'];
     }
-
-    public function getTheme(){
-        return $this->theme;
-    }
-
-    public function toggleTheme(){
-        $_ENV['THEME'] = 'DARK';
-    }
-
 }
 
--- .aw/src/controller/pageloader/PageController.php
+++ .aw/src/controller/pageloader/PageController.php
@@ -5,6 +5,7 @@ namespace awave\pageloader;
 use awave\config\Config;
 use awave\directory\DirectoryReader;
 use Twig\Environment;
+use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
 
@@ -18,7 +19,10 @@ class PageController
     public function __construct()
     {
         $this->loader = new FilesystemLoader('.aw/src/pages', getcwd());
-        $this->twig = new Environment($this->loader);
+        $this->twig = new Environment($this->loader, [
+            'debug' => true
+        ]);
+        $this->twig->addExtension(new DebugExtension());
         $this->directoryReader = new DirectoryReader(__DIR__);
         $this->config = new Config();
     }
@@ -29,6 +33,7 @@ class PageController
             if ($this->directoryReader->projectNameExistsInRootDirectory($_GET['project'])){
                 echo $this->twig->render('project.html.twig',
                     [
+                        "navigation_root_snippet" => $this->getNavigationRootSnippet(),
                         "name" => $_GET['project'],
                         "content" => $this->directoryReader->getProjectContent($_GET['project']),
                         "dir_data" => $this->directoryReader->getDirData($_GET['project']),
@@ -40,14 +45,24 @@ class PageController
             echo $this->twig->render('index.html.twig',
                 [
                     "projects" => $this->directoryReader->getAllProjectsFromRootDirectory(),
-                    "theme" => $this->config->getTheme(),
                 ]);
         } else{
             echo $this->twig->render('index.html.twig',
                 [
+                    "navigation_root_snippet" => $this->getNavigationRootSnippet(),
                     "projects" => $this->directoryReader->getAllProjectsFromRootDirectory(),
-                    "theme" => $this->config->getTheme(),
                 ]);
         }
+    }
+
+    private function getNavigationRootSnippet(){
+        $fullRootDirectoryPath = $_SERVER['DOCUMENT_ROOT'];
+        $rootDirectorySnippets = explode('/', $fullRootDirectoryPath);
+        if (count($rootDirectorySnippets) > 2){
+            $sliced = array_slice($rootDirectorySnippets, -2, 2, true);
+            array_unshift($sliced, '...');
+            return $sliced;
+        }
+        return $rootDirectorySnippets;
     }
 }
